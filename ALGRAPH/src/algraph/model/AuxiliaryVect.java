d0fdc98658c627be14a8983385ade01fbf422f36
Matteo Mele <matteo.meleDev0998@gmail.com>
2018-06-01 12:30:26 +0200

    add structure. Work on model

---
+++ ALGRAPH/src/algraph/model/AuxiliaryVect.java
@@ -0,0 +1,5 @@
+package algraph.model;
+
+public class AuxiliaryVect {
+
+}
---
+++ ALGRAPH/src/algraph/model/Main.java
@@ -0,0 +1,11 @@
+package algraph.model;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+       Graph g = new Graph(10,true);
+       
+       g.setEdge(0, 1, 50);
+       
+       System.out.println(g.adj(0));
+    }
+}
